9ecff1f3f6620ffe3b20efb88afe774082e100e4
Jayan Jacob <jayandevjacob@gmail.com>
2014-04-16 22:41:53 -0700

    fixed specs

--- lib/xml_validator/html_extender.rb
+++ lib/xml_validator/html_extender.rb
@@ -2,46 +2,25 @@ require 'nokogiri'
 
 module XmlValidator
   class HtmlExtender
+    extend Transformer
+    attr_reader :node
     def initialize(node)
       @node = node
     end
 
     def self.convert(tag, opts = {})
       @convert_table ||= {}
+      p "#{self.class} #{self.superclass}------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
       @convert_table[tag.to_s] = opts
+      p @convert_table.inspect
     end
 
     def self.in_conversion_table(tag)
       @convert_table[tag.to_s]
     end
 
-    def expand(_node) 
-      if _node.is_a?(Nokogiri::XML::Element) 
-        return node_expand(_node)
-      elsif _node.is_a?(Nokogiri::XML::NodeSet)
-        return _node.inject('') { |str, ele| str + expand(ele) }
-      elsif  _node.is_a?(Nokogiri::XML::Text)
-        return _node.to_s
-      end
+    def inner_html
+      self.class.transform(node)
     end
-
-    def node_expand(_node)
-      if self.class.in_conversion_table(_node.name)
-        open_tag = self.class.in_conversion_table(_node.name)[:open]
-        close_tag = self.class.in_conversion_table(_node.name)[:close]
-        "#{open_tag}#{expand(_node.children)}#{close_tag}"
-      else
-        "<#{_node.name}#{node_attributes(_node)}>#{expand(_node.children)}</#{_node.name}>"
-      end
-    end
-
-    def node_attributes(_node)
-      if _node.attributes.any?
-        _node.attributes.collect { |key, val| " #{key}=\"#{val}\""}.join('')
-      else
-        ''
-      end
-    end
-
   end
 end
--- spec/lib/xml_validator/html_extender_spec.rb
+++ spec/lib/xml_validator/html_extender_spec.rb
@@ -19,42 +19,23 @@ describe XmlValidator::HtmlExtender do
     end
   end
 
-  describe "#expand" do
+  describe "#inner_html" do
     it "returns the inner html" do
       node = Nokogiri.parse('<p class="pi"><i>a1</i><b><i class="ii">a</i>2</b></p>').children.first
-      Redbold.new(node).expand(node).should eql('<p class="pi"><i>a1</i><b><i class="ii">a</i>2</b></p>')
+      Redbold.new(node).inner_html.should eql('<p class="pi"><i>a1</i><b><i class="ii">a</i>2</b></p>')
     end
 
     context "with conversion tags exist" do
       it "returns the converted inner html" do
         node = Nokogiri.parse('<p class="pi"><red>a1</red><b><i class="ii">a</i>2</b></p>').children.first
-        BigRedbold.new(node).expand(node).should eql('<p class="pi"><span class="bigred">a1</span><b><i class="ii">a</i>2</b></p>')
-        Redbold.new(node).expand(node).should eql('<p class="pi"><span class="red">a1</span><b><i class="ii">a</i>2</b></p>')
+        BigRedbold.new(node).inner_html.should eql('<p class="pi"><span class="bigred">a1</span><b><i class="ii">a</i>2</b></p>')
+        Redbold.new(node).inner_html.should eql('<p class="pi"><span class="red">a1</span><b><i class="ii">a</i>2</b></p>')
       end
     end
 
     it "returns the inner html" do
       node = Nokogiri.parse('<p class="pi"><i>a1</i><b><i class="ii">a</i>2</b></p>').children.first
-      Redbold.new(node).expand(node).should eql('<p class="pi"><i>a1</i><b><i class="ii">a</i>2</b></p>')
+      Redbold.new(node).inner_html.should eql('<p class="pi"><i>a1</i><b><i class="ii">a</i>2</b></p>')
     end
   end
-
-  describe "#node_attributes" do
-    let(:redbold) { Redbold.new('')}
-    it "returns empty string when there are no attributes" do
-      node = Nokogiri.parse('<span>xyz</span>').children.first
-      redbold.node_attributes(node).should eql('')
-    end
-
-    it "returns attributes as string when there is exactly one attribute" do
-      node = Nokogiri.parse('<span class="red">xyz</span>').children.first
-      redbold.node_attributes(node).should eql(' class="red"')
-    end
-
-    it "returns attributes as string when there are more than one attributes" do
-      node = Nokogiri.parse('<span class="red" data="rd">xyz</span>').children.first
-      redbold.node_attributes(node).should eql(' class="red" data="rd"')
-    end
-  end
-
 end
